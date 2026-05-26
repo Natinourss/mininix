a1d4fd70714b0af5003bfdb789643333201e6610
Natinourss <nathanorriols30@gmail.com>
2026-05-27 01:22:36 +0200

    nixvim-config+pentest+obsidian

--- home/programs/nixvim.nix
+++ home/programs/nixvim.nix
@@ -1,10 +1,12 @@
+#nixvim.nix
 { pkgs, ... }:
 
 {
   programs.nixvim = {
     enable = true;
-
     defaultEditor = true;
+
+    globals.mapleader = " ";
 
     colorschemes.catppuccin.enable = true;
 
@@ -14,23 +16,57 @@
       shiftwidth = 2;
       tabstop = 2;
       expandtab = true;
+      clipboard = "unnamedplus";
     };
+
+    clipboard.providers.xclip.enable = true;
+
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>e";
+        action = ":NvimTreeToggle<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = ":Telescope find_files<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>fg";
+        action = ":Telescope live_grep<CR>";
+      }
+      {
+        mode = "n";
+        key = "<leader>tt";
+        action = ":ToggleTerm<CR>";
+      }
+    ];
 
     plugins = {
       # UI
       lualine.enable = true;
       web-devicons.enable = true;
 
-      # file explorer
+      # navigation
       nvim-tree.enable = true;
-
-      # fuzzy finder
       telescope.enable = true;
 
       # syntax
       treesitter = {
         enable = true;
         nixvimInjections = true;
+
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+          bash
+          python
+          lua
+          json
+          yaml
+          regex
+          markdown
+        ];
       };
 
       # LSP
@@ -40,7 +76,10 @@
         servers = {
           nixd.enable = true;
           lua_ls.enable = true;
-          ts_ls.enable = true;
+
+          # pentest core
+          pyright.enable = true;
+          bashls.enable = true;
         };
       };
 
@@ -49,6 +88,46 @@
         enable = true;
         autoEnableSources = true;
       };
+
+      # git
+      gitsigns.enable = true;
+
+      # utils
+      comment.enable = true;
+      surround.enable = true;
+
+      # terminal intégré
+      toggleterm = {
+        enable = true;
+        settings = {
+          direction = "float";
+        };
+      };
+
+      # OBSIDIAN
+      obsidian = {
+        enable = true;
+
+        settings = {
+          workspaces = [
+            {
+              name = "notes";
+              path = "~/notes";
+            }
+          ];
+
+          completion = {
+            nvim_cmp = true;
+          };
+
+          new_notes_location = "notes_subdir";
+
+          daily_notes = {
+            folder = "daily";
+          };
+        };
+      };
     };
   };
 }
+
