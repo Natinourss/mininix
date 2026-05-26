9a894b226ecaf1f8574aae693388108b104999d9
Natinourss <nathanorriols30@gmail.com>
2026-05-27 01:10:04 +0200

    nixvim-config

--- home/programs/nixvim.nix
+++ home/programs/nixvim.nix
@@ -1,5 +1,54 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  programs.nixvim.enable = true;
+  programs.nixvim = {
+    enable = true;
+
+    defaultEditor = true;
+
+    colorschemes.catppuccin.enable = true;
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+    };
+
+    plugins = {
+      # UI
+      lualine.enable = true;
+      web-devicons.enable = true;
+
+      # file explorer
+      nvim-tree.enable = true;
+
+      # fuzzy finder
+      telescope.enable = true;
+
+      # syntax
+      treesitter = {
+        enable = true;
+        nixvimInjections = true;
+      };
+
+      # LSP
+      lsp = {
+        enable = true;
+
+        servers = {
+          nixd.enable = true;
+          lua_ls.enable = true;
+          ts_ls.enable = true;
+        };
+      };
+
+      # autocomplete
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+      };
+    };
+  };
 }
