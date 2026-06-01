9952729ea313e208514719e33dddd80d39e878fb
Natinourss <nathanorriols30@gmail.com>
2026-06-01 18:47:50 +0200

    btop-nvidia-amd-intel

--- home/programs/btop.nix
+++ home/programs/btop.nix
@@ -8,6 +8,19 @@
     settings = {
       theme_background = false;
       truecolor = true;
+
+      # GPU
+      shown_gpus = "nvidia amd intel";
+      nvml_measure_pcie_speeds = true;
+      rsmi_measure_pcie_speeds = true;
+      gpu_mirror_graph = true;
     };
   };
+
+  # Backend GPU
+  home.packages = with pkgs; [
+    # nvtopPackages.nvidia # Nvidia uniquement
+    # full support multi machines :
+    nvtopPackages.full
+  ];
 }
